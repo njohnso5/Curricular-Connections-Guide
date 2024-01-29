e71fe6d2ce99083997157d7a75175e2ecdd639ad
Adam Malik <ahmalik@ncsu.edu>
2024-01-29 11:14:58 -0500

    Admin Manage Themes Tab Created
    Manage Theme Page Currently in Development

---
+++ frontend/src/pages/ThemePage.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import themesService from '../services/themes-service';
+import { Modal, ModalButton, ModalNewSemesterBody, DeleteSemesterModalButton, DeleteSemesterBody } from "../components/Modal.tsx";
+
+const ThemePage: React.FC = () => {
+    return {
+        
+    }
+}
+
+export defualt ThemePage;
