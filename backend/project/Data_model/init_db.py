c0c01687f63ba3851c62c60dd52f1ef61163e9a3
gperezb <gperezb@ncsu.edu>
2024-02-12 11:10:09 -0500

    added my unity id to admin list

--- backend/project/Data_model/init_db.py
+++ backend/project/Data_model/init_db.py
@@ -24,6 +24,7 @@ def create_admins(*args, **kwargs):
         Administrator(unity_id="arosas2", role_id=1),
         Administrator(unity_id="acsawyer", role_id=1),
         Administrator(unity_id="deflower", role_id=1),
+        Administrator(unity_id="gperezb", role_id=1),
         Administrator(unity_id="ahmalik", role_id=1),
         Administrator(unity_id="xce", role_id=1),
         Administrator(unity_id="bswalia", role_id=1)
