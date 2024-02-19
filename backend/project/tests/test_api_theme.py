b727fa1ffed9ddc155cd04d40575e600d79a9509
Adam Malik <ahmalik@ncsu.edu>
2024-02-19 09:56:25 -0500

    Update test_api_theme.py

--- backend/project/tests/test_api_theme.py
+++ backend/project/tests/test_api_theme.py
@@ -7,7 +7,7 @@ THEME_BASE_URL = "v1/themes"
 
 def test_post(client: FlaskClient, app: Flask):
     response = client.post("/v1/administrators/", json=json.loads('{"unity_id":"test", "role_id":"1"}'))
-    assert response.status.code == 200
+    assert response.status_code == 200
 
     response = client.post("/v1/themes/", data={ "name": "Test" })
-    assert response.status.code == 200
+    assert response.status_code == 200
