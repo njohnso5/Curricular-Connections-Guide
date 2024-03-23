3bdc0e3af798328542532b67ce3ae1f3c8c68e15
Adam Malik <ahmalik@ncsu.edu>
2024-03-23 17:42:13 -0400

    Updated Logging Script

--- backend/project/Controllers/theme.py
+++ backend/project/Controllers/theme.py
@@ -28,8 +28,9 @@ class ThemeList(MethodView):
         theme.name = theme_data.get("name")  
     # Finish building the theme object and add it to the db
         try:
-            call = str(theme_dao.insert(theme))
-            logging.Logging.logAPI(self, call)
+            theme_dao.insert(theme)
+            call = "POST /v1/themes/ HTTP/1.1 200"
+            logging.Logging.prepend_line('log.txt', call)
         except SQLAlchemyError:
             abort(500, message="An error occured inserting the theme")
         except ArgumentError:
@@ -47,6 +48,8 @@ def handle_theme_id(theme_id):
     if request.method == "DELETE":
         # If the request method is DELETE, delete the theme by its ID
         theme_dao.delete(theme_id)
+        call = "DELETE /v1/themes/" + str(theme_id) + "/ HTTP/1.1 200"
+        logging.Logging.prepend_line('log.txt', call)
 
         # Return a JSON response indicating successful deletion and a status code of 200
         return make_response(jsonify({"success": "Theme deleted"}), 200)
