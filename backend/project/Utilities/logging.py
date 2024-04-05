df89368d2026dcf1db6a3b7475380350b18d707f
Adam Malik <ahmalik@ncsu.edu>
2024-04-05 11:49:26 -0400

    More Logging Points Added and logQuery function created

--- backend/project/Utilities/logging.py
+++ backend/project/Utilities/logging.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 import Data_model.adminlog_dao as adminlog_dao
+import Data_model.userlog_dao as userlog_dao
 from sqlalchemy.exc import SQLAlchemyError
 from flask_smorest import abort
 from flask import g
@@ -14,5 +15,14 @@ def logAPI(log):
         log.datetime = now
         #insert the log to admin log table
         adminlog_dao.insert(log)
+    except SQLAlchemyError:
+        abort(500, message="An error occured inserting the log")
+
+def logQuery(log):
+    try:
+        tz = timezone('EST')
+        now = datetime.datetime.now(tz)
+        log.datetime = now
+        userlog_dao.insert(log)
     except SQLAlchemyError:
         abort(500, message="An error occured inserting the log")
